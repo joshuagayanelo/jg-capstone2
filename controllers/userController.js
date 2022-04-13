ad4858aad98ba378626fd4c2be598180b8b808e7
Joshua Ren Gayanelo <joshuagayanelo@gmail.com>
2022-04-14 00:49:18 +0800

    added feature enhancements

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,70 +1,44 @@
+
 const User = require('../models/User');
 const bcrypt = require('bcrypt'); /*Password encryption*/
 const auth = require('../auth');
 
 
-// Check if the email already exists
-/*
-	Business logic:
-	1. Use mongoose "find" method to find duplicate emails.
-	2. Use the "then" method to send a response back to the client.
-*/
-/*module.exports.checkEmailExists = (reqBody) => {
-	return User.find({email: reqBody.email }).then(result => {
-		if(result.length > 0) {
-			return true;
-		} else {
-			// No duplicate email found
-			return false;
-		}
-	})
-}*/
-
-/*User Registration
-
-	Business Logic
-	1. Create a new User Object.
-	2. Make sure that the password is encrypted.
-	3. Save the new User to the database.
-*/
-
+// REGISTER USER
 module.exports.registerUser = (reqBody) => {
-	// Creates a New User Object
-	let newUser = new User ({
-		firstName: reqBody.firstName,
-		lastName: reqBody.lastName,
-		mobileNo: reqBody.mobileNo,
-		email: reqBody.email,
-		password: bcrypt.hashSync(reqBody.password, 10) 
+	
+	return User.findOne({ email:reqBody.email }).then(result => {
+		if(result === null) {
 
-	})
+			let newUser = new User ({
+				firstName: reqBody.firstName,
+				lastName: reqBody.lastName,
+				mobileNo: reqBody.mobileNo,
+				email: reqBody.email,
+				password: bcrypt.hashSync(reqBody.password, 10) 
+			})
 
-	return newUser.save().then((user,error) => {
-		// User registration failed.
-		if(error){
-			return false;
+			return newUser.save().then((user,error) => {
+				// User registration failed.
+				if(error){
+					return false;
+				} else {
+					// User registration successful.
+					return {message:"User successfully registered."};
+				}
+			})
+
 		} else {
-			// User registration successful.
-			return true;
+			return {message: "User already exists."}	
 		}
 	})
 };
 
-// Get all uers
-module.exports.getAllUser = (reqBody) => {
-	return User.find({}).then((result, err) => {
-		if(err) {
-			return false
-		} else {
-			return result;
-		}
-	})
-};
 
-// Login User
+// LOGIN USER
 module.exports.loginUser = (reqBody) => {
 
-	return User.findOne({email: reqBody.email}).then(result =>{
+	return User.findOne({email: reqBody.email}).then(result => {
 		if(result == null) {
 			return false;
 		} else {
@@ -77,6 +51,68 @@ module.exports.loginUser = (reqBody) => {
 				// Password does not match
 				return false;
 			}
+		}
+	})
+};
+
+
+// GET ALL USERS
+module.exports.getAllUser = (reqBody) => {
+	
+	return User.find({ isAdmin: false }).then((result, err) => {
+
+		if(err) {
+			return false;
+		} else {
+
+			return result;
+		}
+	})
+};
+
+
+// GET ALL ADMIN
+module.exports.getAdmin = (reqBody) => {
+	
+	return User.find({ isAdmin: true }).then((result, err) => {
+		if(err) {
+			return false;
+		} else {
+			return result
+		}
+	})
+};
+
+
+// ASSIGN USER AS ADMIN
+module.exports.setAdmin = (id, res) => {
+	
+	let newStatus = {
+		isAdmin: true
+	}
+
+	return User.findByIdAndUpdate(id, newStatus).then((result, err) => {
+		if(result) {
+			return {message: "User successfully assigned as Admin."}
+		} else {
+			return false;
+		}
+	})
+};
+
+
+// ASSIGN USER AS ADMIN
+module.exports.setUser = (id, res) => {
+	
+	let newStatus = {
+		isAdmin: false
+	}
+
+	return User.findByIdAndUpdate(id, newStatus).then((result, err) => {
+		if(result) {
+			return {message: "User successfully assigned as User."}
+		} else {
+			return false;
 		}
 	})
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,28 +1,80 @@
 
 const express = require('express');
 const router = express.Router();
-// const auth = require('../auth');
+const auth = require('../auth');
 
 const UserController = require('../controllers/userController');
 
 
-// Registration for User
+// REGISTER USER
 router.post('/register', (req, res) => {
 	UserController.registerUser(req.body).then(result => res.send(result))
 });
 
-// Get all Users
+
+// GET ALL USERS
 router.get('/', (req, res) => {
-	UserController.getAllUser(req.body).then(result => res.send(result))
+
+	const data = {
+		isAdmin: auth.decode(req.headers.authorization).isAdmin
+	}
+
+	if(data.isAdmin) {
+		UserController.getAllUser(req.body).then(result => res.send(result))
+	} else {
+		res.send("Invalid token.")
+	}
 });
 
-// Login a user
+
+// GET ALL ADMIN
+router.get('/admin', (req, res) => {
+	
+	const data = {
+		isAdmin: auth.decode(req.headers.authorization).isAdmin
+	}
+
+	if(data.isAdmin) {	
+		UserController.getAdmin(req.body).then(result => res.send(result))
+	} else {
+		res.send("Invalid token.")
+	}
+});
+
+
+// LOGIN USER
 router.post('/login', (req ,res) => {
 	UserController.loginUser(req.body).then(result => res.send(result))
 });
 
-router.put('/set-to-admin/:id', (req, res) => {
-	UserController.makeAdmin(req.params.id).then(result => res.send(result))
+
+// ASSIGN USER AS ADMIN
+router.put('/set-admin/:id', auth.verify, (req, res) => {
+	
+	const data = {
+		isAdmin: auth.decode(req.headers.authorization).isAdmin
+	}
+
+	if(data.isAdmin){
+		UserController.setAdmin(req.params.id).then(result => res.send(result));
+	} else {
+		res.send("Invalid token.")
+	}
+});
+
+
+// ASSIGN USER AS ADMIN
+router.put('/set-user/:id', auth.verify, (req, res) => {
+	
+	const data = {
+		isAdmin: auth.decode(req.headers.authorization).isAdmin
+	}
+
+	if(data.isAdmin){
+		UserController.setUser(req.params.id).then(result => res.send(result));
+	} else {
+		res.send("Invalid token.")
+	}	
 });
 
 module.exports = router;
